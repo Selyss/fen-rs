2ba76e2105d820ebd260fa7ec3a2571132db5f5b
Selyss <99344963+Selyss@users.noreply.github.com>
2024-04-30 19:29:58 -0400

    custom error

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,32 @@
+use std::fmt;
 use std::fmt::Error;
 
 pub const STARTING_FEN: &str = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+
+#[derive(Debug)]
+pub enum FenError {
+    NotEnoughFields(String),
+    BadPieceNotation(String),
+    BadActiveColor(String),
+    BadEnPassant(String),
+    BadHalfmove(String),
+    BadFullmove(String),
+}
+
+impl fmt::Display for FenError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            FenError::NotEnoughFields(msg) => write!(f, "Not enough FEN fields: {}", msg),
+            FenError::BadPieceNotation(msg) => write!(f, "Piece notation error: {}", msg),
+            FenError::BadActiveColor(msg) => write!(f, "Active color error: {}", msg),
+            FenError::BadEnPassant(msg) => write!(f, "En passant error: {}", msg),
+            FenError::BadHalfmove(msg) => write!(f, "Halfmove error: {}", msg),
+            FenError::BadFullmove(msg) => write!(f, "Fullmove error: {}", msg),
+        }
+    }
+}
+
+impl std::error::Error for FenError {}
 
 /// Piece options and color piece belongs to
 #[derive(Clone, Debug, Eq, PartialEq)]
