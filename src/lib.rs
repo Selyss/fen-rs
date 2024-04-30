c31317c34a3825a7daef269f8e68cd7ea223cde2
Selyss <99344963+Selyss@users.noreply.github.com>
2024-04-30 14:11:09 -0400

    update parsing

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,8 @@
 use std::fmt::Error;
 
-/// Piece options and color piece belongs to
+pub const STARTING_FEN: &str = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
 
+/// Piece options and color piece belongs to
 #[derive(Clone, Debug, Eq, PartialEq)]
 pub enum Color {
     White,
@@ -26,7 +27,7 @@ pub struct Piece {
 
 /// The state of the game
 #[derive(Clone, Debug, Eq, PartialEq)]
-pub struct Board {
+pub struct BoardState {
     // For each square, a piece can either exist or be None
     pub piece_placement: Vec<Option<Piece>>,
     pub active_color: Color,
@@ -45,10 +46,68 @@ pub struct Board {
     pub fullmove_clock: u32,
 }
 
-impl Board {
-    pub fn from_fen(fen: &str) -> Result<Board, Error> {
+impl BoardState {
+    pub fn new() -> BoardState {
+        let starting_board = Self::create_board(STARTING_FEN);
+        return BoardState {
+            piece_placement: starting_board,
+            active_color: Color::White,
+            white_castle_kingside: true,
+            white_castle_queenside: true,
+            black_castle_kingside: true,
+            black_castle_queenside: true,
+            en_passant_target: None,
+            halfmove_clock: 0,
+            fullmove_clock: 1,
+        };
+    }
+
+    pub fn from_fen(fen: &str) -> Result<BoardState, Error> {
+        let mut fen_struct = BoardState::new();
         let fields: Vec<_> = fen.split(" ").collect();
-        let board = fields[0];
+        let board = Self::create_board(fields[0]);
+        fen_struct.piece_placement = board;
+
+        let active_color = match fields[1] {
+            "w" => Color::White,
+            "b" => Color::White,
+            _ => panic!("Error: failed to parse active color"),
+        };
+        fen_struct.active_color = active_color;
+
+        let castling = fields[2];
+        // maybe extract into function later
+        fen_struct.white_castle_kingside = castling.contains('K');
+        fen_struct.white_castle_queenside = castling.contains('Q');
+        fen_struct.black_castle_kingside = castling.contains('k');
+        fen_struct.black_castle_queenside = castling.contains('q');
+
+        let en_passant = match fields[3] {
+            // TODO:
+            _ => None,
+        };
+
+        fen_struct.en_passant_target = en_passant;
+
+        let halfmove = fields[4]
+            .parse::<u32>()
+            .expect("Error: failed to parse halfmove clock");
+
+        fen_struct.halfmove_clock = halfmove;
+
+        let fullmove = fields[5]
+            .parse::<u32>()
+            .expect("Error: failed to parse fullmove number");
+
+        fen_struct.fullmove_clock = fullmove;
+
+        println!("{:?}", fen_struct);
+
+        todo!()
+    }
+
+    // board portion of the fen
+    fn create_board(board: &str) -> Vec<Option<Piece>> {
         let board: Vec<_> = board.split("/").collect();
         let mut new_board = Vec::new();
         for file in board {
@@ -56,9 +115,7 @@ impl Board {
                 new_board.push(Self::get_piece(square));
             }
         }
-        println!("{:?}", new_board);
-
-        todo!()
+        new_board
     }
 
     fn get_piece(piece: char) -> Option<Piece> {
